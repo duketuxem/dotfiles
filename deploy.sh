09a86449f7009c35e9a791d1228644076ad63e83
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2022-02-18 23:41:23 +0100

    run

---
+++ deploy.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# [package manager] update && upgrade
+# [package manager] install git
+# ~ $> mkdir -p workspace/ricing && cd workspace/ricing
+# git clone monrepo/dotfiles && cd mon_repo
+#
+# ./deploy.sh
+
+# Requirements for this script:
+# 
+# sudo
+# a package manager
+
+#install_freebsd_dependencies() {
+#
+#}
+
+install_void_dependencies() {
+        # [~/workspace/ricing/dotfiles] - Install dependencies
+
+	printf "Installing all the dependencies for the Rice...\n"
+
+        sudo xbps-install curl base-devel \
+        libX11-devel libXft-devel libXinerama-devel \
+        fonts-droid-ttf \
+	&& printf "Dependencies successfully installed!\n\n"
+}
+
+install_suckless_suite() {
+        #cd ..
+	# [~/workspace/ricing]
+
+	printf "Installing the suckless software suite...\n"
+        git clone https://github.com/DukeTuxem/dwm.git \
+        && cd dwm && git checkout my_fork \
+        && sudo make install && cd .. && \
+
+        git clone https://github.com/DukeTuxem/st.git \
+        && cd st && git checkout my_fork \
+        && sudo make install && cd .. && \
+        
+	#&& git checkout my_fork \
+        git clone https://github.com/DukeTuxem/dmenu.git \
+        && cd dmenu && sudo make install && cd .. \
+	&& printf "Suckless suite successfully installed!\n\n" \
+	|| printf "Some suckless program could not have been installed :(\n\n"
+}
+
+# For now just one
+install_custom_fonts() {
+	printf "Assert the custom font directory is existing..."
+	#cd && mkdir -p .local/share/fonts/ && cd .local/share/fonts
+	mkdir -p .local/share/fonts/ && cd .local/share/fonts
+	
+	printf "Downloading Hack Nerd Font..."
+	curl -fLo "Hack Regular Nerd Font Complete Mono2.ttf" \
+		https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+	printf "Hack installed!"
+}
+
+# Main
+
+install_void_dependencies
+install_suckless_suite 
+install_custom_fonts
+
+#startx /usr/local/bin/dwm
+

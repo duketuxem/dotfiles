4aa11479c196b28d9044d822bcafad015f51d959
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2022-05-01 20:27:43 +0200

    Pursuing on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,72 +1,90 @@
 #!/bin/sh
 
+# == Void Linux ==
+# sudo + xbps
+
+# Off repository required actions
 # [package manager] update && upgrade
 # [package manager] install git
-# ~ $> mkdir -p workspace/ricing && cd workspace/ricing
-# git clone monrepo/dotfiles && cd mon_repo
-#
-# ./deploy.sh
+# -----------------------------------
 
-# Requirements for this script:
-# 
-# sudo
-# a package manager
 
-#install_freebsd_dependencies() {
-#
-#}
+TUNING_DIR="$HOME/workspace/ricing"
 
+
+# utils/wrappers:
+call() {
+    echo "$@"
+
+    "$@"
+    ret=$?
+    if [ $ret -ne 0 ]
+    then
+        echo "Error: Command [ $@ ]\nreturned $ret"
+        exit $ret
+    fi
+}
+
+
+create_arborescence() {
+    call cd
+    call mkdir -p "$TUNING_DIR"
+    call cd "$TUNING_DIR"
+}
 
 install_void_dependencies() {
-        # [~/workspace/ricing/dotfiles] - Install dependencies
+    printf "Installing all the dependencies for the Rice...\n"
 
-	printf "Installing all the dependencies for the Rice...\n"
+    # compton
 
-	# compton
-        sudo xbps-install curl base-devel xorg \
-        libX11-devel libXft-devel libXinerama-devel \
-        fonts-droid-ttf \
-	&& printf "Dependencies successfully installed!\n\n"
+    sudo xbps-install curl base-devel xorg \
+    # dwm dependencies
+    libX11-devel libXft-devel libXinerama-devel \
+    # font support
+    noto-fonts-cjk \
+    && printf "Dependencies successfully installed!\n\n"
 }
 
 install_suckless_suite() {
-	# Leaving dotfiles repo...
-        cd ..
+    # Leaving dotfiles repo...
+    cd ..
 
-	printf "Installing the suckless software suite...\n"
-        git clone https://github.com/DukeTuxem/dwm.git \
-        && cd dwm && git checkout my_fork \
-        && sudo make install && cd .. && \
+    printf "Installing the suckless software suite...\n"
+    git clone https://github.com/DukeTuxem/dwm.git \
+    && cd dwm && git checkout my_fork \
+    && sudo make install && cd .. && \
 
-        git clone https://github.com/DukeTuxem/st.git \
-        && cd st && git checkout my_fork \
-        && sudo make install && cd .. && \
-        
-	#&& git checkout my_fork \
-        git clone https://github.com/DukeTuxem/dmenu.git \
-        && cd dmenu && sudo make install && cd .. \
-	&& printf "Suckless suite successfully installed!\n\n" \
-	|| printf "Some suckless program could not have been installed :(\n\n"
+    git clone https://github.com/DukeTuxem/st.git \
+    && cd st && git checkout my_fork \
+    && sudo make install && cd .. && \
+
+    #&& git checkout my_fork \
+    git clone https://github.com/DukeTuxem/dmenu.git \
+    && cd dmenu && sudo make install && cd .. \
+    && printf "Suckless suite successfully installed!\n\n" \
+    || printf "Some suckless program could not have been installed :(\n\n"
 }
 
 # For now just one
 install_custom_fonts() {
-	printf "Assert the custom font directory is existing..."
-	cd && mkdir -p .local/share/fonts/ && cd .local/share/fonts
-	
-	printf "Downloading Hack Nerd Font..."
-	curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" \
-		https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+    printf "Assert the custom font directory is existing..."
+    cd && mkdir -p .local/share/fonts/ && cd .local/share/fonts
 
-	fc-cache -fv ~/.local/share/fonts
-	#if fc-match ... :
-	printf "Hack installed!\n"
+    printf "Downloading Hack Nerd Font..."
+    curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" \
+            https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+
+    fc-cache -fv ~/.local/share/fonts
+    #if fc-match ... :
+    printf "Hack installed!\n"
 }
 
 # Main
-
 install_void_dependencies
-install_suckless_suite 
-install_custom_fonts
 
-printf "Welcome home...\n"
+create_directory_structure
+
+install_suckless_suite
+install_custom_fonts
+#
+#printf "Welcome home...\n"

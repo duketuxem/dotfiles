eadf5590f2e21ee9662c29b4ee46c5e8956468c1
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2024-05-31 22:53:56 +0200

    core config

--- home/run.sh
+++ home/run.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 
-chezmoi execute-template < ./run_install-packages.sh.tmpl > script.sh && ./script.sh
+chezmoi execute-template < ./run_install-packages.sh.tmpl > script.sh && \
+	chmod +x script.sh && ./script.sh

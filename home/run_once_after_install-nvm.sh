a50ca59f92d0e8f40353fe7e0a1cba8d44cd0dbf
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2025-01-30 11:18:53 +0100

    add diff-so-fancy (+config git/chezmoi) + nvm + fix vim ale

---
+++ home/run_once_after_install-nvm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Set some executable scripts
+chmod +x "$HOME"/.local/bin/diff-so-fancy
+
+# Do not alter any user shell configuration thanks to $PROFILE
+PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash'
+
+# NVM_DIR should normally be set but still just in case
+export NVM_DIR="${NVM_DIR:-$HOME/config/nvm}"
+
+# Source the nvm 'env' so `node` commands will be available
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts

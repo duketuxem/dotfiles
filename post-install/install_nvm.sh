64447640f88e9d06a25d19a64eab57cf1589f7c4
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2025-03-25 21:06:45 +0100

    post: nvm: comments and sort installs

--- post-install/install_nvm.sh
+++ post-install/install_nvm.sh
@@ -5,13 +5,15 @@ PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm
 
 # Note:
 # Even if NVM_DIR is set in zshrc, nvm.sh does not exist, so it is not sourced.
-# After a fresh install, it needs to be sourced for the next commands to work.
-# Every other time is handled in zshrc with that same instruction:
+# After a fresh install, it needs to be sourced for the next commands to work,
+# and for this running script only.
+# Every other time is handled in zshrc with that same following instruction,
+# and this is why it tells you reload your shell...
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Support until 2027
 nvm install --lts=jod && \
-	npm install -g typescript \
 	npm install -g bash-language-server \
+	npm install -g typescript \
 	npm install -g typescript-language-server \
 	npm install -g vim-language-server

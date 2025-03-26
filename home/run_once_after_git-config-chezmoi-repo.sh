f963cfc58830aac774f463b0524abe1d3fdfcea2
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2025-03-26 11:12:49 +0100

    run_once_ scripts working

--- home/run_once_after_git-config-chezmoi-repo.sh
+++ home/run_once_after_git-config-chezmoi-repo.sh
@@ -4,6 +4,8 @@ git_username="duketuxem"
 git_name="Duke Tuxem"
 git_mail="33418846+DukeTuxem@users.noreply.github.com"
 
+cd "$HOME/.local/share/chezmoi" || exit 1
+
 git remote set-url origin "git@github.com:$git_username/dotfiles.git" && \
 echo "Repository URL set to use ssh with username: '$git_username'"
 
@@ -13,5 +15,6 @@ echo "Repository user name set to: '$git_name'"
 git config user.email "$git_mail" && \
 echo "Repository user mail set to: '$git_mail'"
 
-echo "hint: If you need, update the variables and re-run this script with 'sh $0'."
+echo "hint: If you need, update the variables and re-run this script:"
+echo "sh run_once_after_git-config-chezmoi-repo.sh"
 
